d083441ea6f2a1d7733805eee5cdcc59dfe185c1
Filip Peterek <filip.peterek@firma.seznam.cz>
2022-06-17 21:37:06 +0200

    Non-null args

--- src/main/kotlin/lsp/TILScriptTextDocumentService.kt
+++ src/main/kotlin/lsp/TILScriptTextDocumentService.kt
@@ -1,25 +1,28 @@
 package org.fpeterek.til.typechecking.lsp
 
-import org.eclipse.lsp4j.DidChangeTextDocumentParams
-import org.eclipse.lsp4j.DidCloseTextDocumentParams
-import org.eclipse.lsp4j.DidOpenTextDocumentParams
-import org.eclipse.lsp4j.DidSaveTextDocumentParams
+import org.eclipse.lsp4j.*
 import org.eclipse.lsp4j.services.TextDocumentService
+import java.util.concurrent.CompletableFuture
 
 class TILScriptTextDocumentService : TextDocumentService {
-    override fun didOpen(params: DidOpenTextDocumentParams?) {
+
+    override fun rename(params: RenameParams): CompletableFuture<WorkspaceEdit> {
+        return super.rename(params)
+    }
+
+    override fun didOpen(params: DidOpenTextDocumentParams) {
         TODO("Not yet implemented")
     }
 
-    override fun didChange(params: DidChangeTextDocumentParams?) {
+    override fun didChange(params: DidChangeTextDocumentParams) {
         TODO("Not yet implemented")
     }
 
-    override fun didClose(params: DidCloseTextDocumentParams?) {
+    override fun didClose(params: DidCloseTextDocumentParams) {
         TODO("Not yet implemented")
     }
 
-    override fun didSave(params: DidSaveTextDocumentParams?) {
+    override fun didSave(params: DidSaveTextDocumentParams) {
         TODO("Not yet implemented")
     }
 }
--- src/main/kotlin/lsp/TILScriptWorkspaceService.kt
+++ src/main/kotlin/lsp/TILScriptWorkspaceService.kt
@@ -5,11 +5,11 @@ import org.eclipse.lsp4j.DidChangeWatchedFilesParams
 import org.eclipse.lsp4j.services.WorkspaceService
 
 class TILScriptWorkspaceService : WorkspaceService {
-    override fun didChangeConfiguration(params: DidChangeConfigurationParams?) {
+    override fun didChangeConfiguration(params: DidChangeConfigurationParams) {
         TODO("Not yet implemented")
     }
 
-    override fun didChangeWatchedFiles(params: DidChangeWatchedFilesParams?) {
+    override fun didChangeWatchedFiles(params: DidChangeWatchedFilesParams) {
         TODO("Not yet implemented")
     }
 }
