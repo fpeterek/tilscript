b148d04e70d9d1c427191972612f1d24ddc652ab
Filip Peterek <fpeterek@seznam.cz>
2023-04-07 20:12:43 +0200

    Better reporting

--- common/common/src/main/kotlin/interpreterinterface/FunctionInterface.kt
+++ common/common/src/main/kotlin/interpreterinterface/FunctionInterface.kt
@@ -21,44 +21,52 @@ sealed class FunctionInterface constructor(
     val signature = FunctionType(returns, argTypes)
     val tilFunction = TilFunction(name, SrcPosition(-1, -1), signature, implementation = this)
 
-    protected fun checkArgCount(fnArgs: List<Construction>) {
+    protected fun checkArgCount(fnArgs: List<Construction>, ctx: FnCallContext) {
         if (fnArgs.size != args.size) {
-            die("Incorrect number of arguments in application of function '$name' (expected: ${args.size}, received: ${fnArgs.size})")
+            die(
+                "Incorrect number of arguments in application of function '$name' (expected: ${args.size}, received: ${fnArgs.size})",
+                ctx.position
+            )
         }
     }
 
-    private fun handleArgMatches(matches: List<Boolean>, fnArgs: List<Type>) {
+    private fun handleArgMatches(matches: List<Boolean>, fnArgs: List<Type>, ctx: FnCallContext) {
         matches.forEachIndexed { idx, match ->
             if (!match) {
                 val exp = argTypes[idx]
                 val rec = fnArgs[idx]
-                die("Invalid argument type in application of function '$name' (expected: $exp, received: $rec)")
+                die(
+                    "Invalid argument type in application of function '$name' (expected: $exp, received: $rec)",
+                    ctx.position,
+                )
             }
         }
     }
 
-    protected fun checkArgTypes(interpreter: InterpreterInterface, fnArgs: List<Type>) = handleArgMatches(
-        interpreter.fnArgsMatch(signature, fnArgs),
-        fnArgs
-    )
+    protected fun checkArgTypes(interpreter: InterpreterInterface, fnArgs: List<Type>, ctx: FnCallContext) =
+        handleArgMatches(
+            interpreter.fnArgsMatch(signature, fnArgs),
+            fnArgs,
+            ctx
+        )
 
-    protected fun checkSignature(interpreter: InterpreterInterface, returned: Type, fnArgs: List<Type>) {
+    protected fun checkSignature(interpreter: InterpreterInterface, returned: Type, fnArgs: List<Type>, ctx: FnCallContext) {
         val (retvalMatch, argsMatches) = interpreter.fnSignatureMatch(signature, returned, fnArgs)
 
         if (!retvalMatch) {
-            die("Type returned by function '$name' does not match expected return type (expected: $returns, received: $returned)")
+            die("Type returned by function '$name' does not match expected return type (expected: $returns, received: $returned)", ctx.position)
         }
 
-        handleArgMatches(argsMatches, fnArgs)
+        handleArgMatches(argsMatches, fnArgs, ctx)
     }
 
     protected fun runWithTypechecks(interpreter: InterpreterInterface, args: List<Construction>, ctx: FnCallContext): Construction {
-        checkArgCount(args)
-        checkArgTypes(interpreter, args.map { it.constructionType })
+        checkArgCount(args, ctx)
+        checkArgTypes(interpreter, args.map { it.constructionType }, ctx)
 
         val retval = apply(interpreter, args, ctx)
 
-        checkSignature(interpreter, retval.constructionType, args.map { it.constructionType })
+        checkSignature(interpreter, retval.constructionType, args.map { it.constructionType }, ctx)
 
         return retval
     }
