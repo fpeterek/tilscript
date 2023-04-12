377c48121b17923fda1dc4c25c68f68e4cbed6e0
Filip Peterek <fpeterek@seznam.cz>
2023-04-12 20:53:39 +0200

    tilscript.sh

---
+++ bin/tilscript.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+DIR_NAME=`dirname $0`
+LIBS=`ls $DIR_NAME/libs | tr '\n' ':' | sed 's/,$//'`
+JAR_NAME="$DIR_NAME/tilscript.jar"
+
+if [ -z '$LIBS' ]; then
+    JARS="$JAR_NAME:$LIBS"
+else
+    JARS="$JAR_NAME"
+fi
+
+java -cp $JARS org.fpeterek.tilscript.interpreter.MainKt "$@"
+
